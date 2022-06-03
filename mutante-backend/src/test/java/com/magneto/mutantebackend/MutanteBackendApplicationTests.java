89443d39aec00399357529a001cc98d93dcffa0d
acastroi <acastroi@dane.gov.co>
2022-06-02 22:03:42 -0500

    Se agrega test para no mutante

--- mutante-backend/src/test/java/com/magneto/mutantebackend/MutanteBackendApplicationTests.java
+++ mutante-backend/src/test/java/com/magneto/mutantebackend/MutanteBackendApplicationTests.java
@@ -20,22 +20,7 @@ class MutanteBackendApplicationTests {
 	@Test
 	void contextLoads() {
 
-		/*
 		String[] arrayDNA = {
-			"ATGCGATTBG",
-			"CAGTGCTCAG",
-			"TTATGTAAGG",
-			"AGAAGGTTGG",
-			"CCCCTACCCC",
-			"TCACTAGCTT",
-			"CACGTAACTT",
-			"GCCAACAACT",
-			"AACCTGACAT",
-			"CCGTCCTTTT"
-		};
-		*/
-
-		String[] arrayDNA1 = {
 			"ATGCGATTBG",
 			"CTGTACTCAA",
 			"TTATGGAAGG",
@@ -50,6 +35,36 @@ class MutanteBackendApplicationTests {
 			"GGTAACAGCT"
 		};
 
+		String[] arrayDNA1 = {
+			"ATGCGATTBG",
+			"CTGTACTCAA",
+			"TTATGGAAGG",
+			"AGAATGTTGG",
+			"CTCCGACCTA",
+			"TCACTAGCTT",
+			"CACGTAAGTT",
+			"GCGAACCCCT",
+			"AACCTGACAA",
+			"CCGTCCTTTA",
+			"GCTAACAGCT",
+			"GGTAACAGCT"
+		};
+
+		// Test mutante
+
+		try {		
+			SecuenciaADNDto secuenciaADNDto = new SecuenciaADNDto();
+			secuenciaADNDto.setDna(arrayDNA);
+
+			ResponseEntity<?> responseEntity = mutanteController.detectarMutante(secuenciaADNDto);
+
+			System.out.println("detectarMutante: " + responseEntity.getStatusCode().value() + " " + responseEntity.getStatusCode().getReasonPhrase());
+		} catch (ResponseStatusException ex) {
+			System.out.println("detectarMutante: " + ex.getMessage());
+		}
+
+		// Test no mutante
+
 		try {		
 			SecuenciaADNDto secuenciaADNDto = new SecuenciaADNDto();
 			secuenciaADNDto.setDna(arrayDNA1);
@@ -61,24 +76,14 @@ class MutanteBackendApplicationTests {
 			System.out.println("detectarMutante: " + ex.getMessage());
 		}
 
+
+		// Test estadisticas
+
 		ResponseEntity<?> responseEntity = mutanteController.consultarEstadisticas();
 
 		System.out.println("consultarEstadisticas: " + ((EstadisticaDto)responseEntity.getBody()).toString());
 		
-		/*
-		boolean resp = mutanteService.isMutant(secuenciaADNDto);
-
-		if (resp) {
-			System.out.println("Si es mutante");
-		}
-		else {
-			System.out.println("No es mutante");
-		}
-
-		EstadisticaDto estadisticaDto = mutanteService.consultarEstadisticas2();
-
-		System.out.println("Ratio: " + estadisticaDto.getRatio());
-		*/
+		
 	}
 
 }
